6210811ba6ca3e21885f683acfb8ea5f80155dcb
hiimkhang <phanvinhkhang@gmail.com>
2023-01-07 01:11:28 +0700

    add users.urls

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+
+app_name = "users"
+urlpatterns = [
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/github/", views.github_login, name="github-login"),
+    path("logout/", views.log_out, name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
+    path("update-password/", views.UpdatePasswordView.as_view(), name="password"),
+    path("switch-hosting/", views.switch_hosting, name="switch-hosting"),
+    # path("verify/<str:key>", views.complete_verification, name="complete-verification"),
+]
